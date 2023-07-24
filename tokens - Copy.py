fbc520493dacc2af1602d771af7d9825b886b98e
Amin <79174535+ImZadeQasemzade@users.noreply.github.com>
2023-07-24 17:24:32 -0400

    Add files via upload

---
+++ tokens - Copy.py
@@ -0,0 +1,23 @@
+TelegramToken = " "
+
+bots_token = {
+    "bot1": {
+        "CONSUMER_KEY": " ",
+        "CONSUMER_SECRET": " ",
+        "ACCESS_TOKEN": " ",
+        "ACCESS_TOKEN_SECRET": " ",
+        "BEARER_TOKEN": " ",
+        "CLIENT_ID": " ",
+        "CLIENT_SECRET": " "
+    },
+
+    "bot2": {
+        "CONSUMER_KEY": " ",
+        "CONSUMER_SECRET": " ",
+        "ACCESS_TOKEN": " ",
+        "ACCESS_TOKEN_SECRET": " ",
+        "BEARER_TOKEN": " ",
+        "CLIENT_ID": " ",
+        "CLIENT_SECRET": " "
+    }
+}
